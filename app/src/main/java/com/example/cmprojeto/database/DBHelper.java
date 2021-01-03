79a63aa47ef31d63d8f95e578ac716f9a8efb9ff
AfonsoCunha22 <64204008+AfonsoCunha22@users.noreply.github.com>
2021-01-03 17:42:45 +0000

    Changes to sessions and Icon

--- app/src/main/java/com/example/cmprojeto/database/DBHelper.java
+++ app/src/main/java/com/example/cmprojeto/database/DBHelper.java
@@ -360,7 +360,6 @@ public class DBHelper{
                                     new LatLng(doc.getDouble("latitude"),doc.getDouble("longitude")),
                                     Objects.requireNonNull(doc.get("description")).toString()));
                 }
-
                 callback.manageUserSession(sessions);
             }
         });
